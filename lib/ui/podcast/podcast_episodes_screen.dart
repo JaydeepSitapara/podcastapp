91dc33f2a68f71b9eba0d32c10262d8fb0ec07e5
Nirbhay_34 <nirbhay.v@devineglobe.com>
2026-01-20 15:19:48 +0530

    podcast app first commit

---
+++ lib/ui/podcast/podcast_episodes_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PodcastEpisodesScreen extends StatefulWidget {
+  const PodcastEpisodesScreen({super.key});
+
+  @override
+  State<PodcastEpisodesScreen> createState() => _PodcastEpisodesScreenState();
+}
+
+class _PodcastEpisodesScreenState extends State<PodcastEpisodesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
